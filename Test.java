8df4fc23baac8f7d38cd9b4899c5cd8e63f5ca2a
BS BARKATHULLA <bsbarkathulla997@gmail.com>
2026-01-28 16:14:17 +0530

    Update12 Test.java

--- Test.java
+++ Test.java
@@ -1,11 +1,11 @@
 public class Test {
-	public static void main(String[] args) {
+
 		// TODO Auto-generated method stub
 		System.out.println("************************************");
 		System.out.println("Hello from Github");
 		System.out.println("Hello to Jenkins");
-		System.out.println"Wassup Buddy!! v10"
+		System.out.println("Wassup Buddy!! v10");
 		System.out.println("************************************");
 		
-	}
+	
 }
